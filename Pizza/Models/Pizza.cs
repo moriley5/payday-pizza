095c328b7fffc6f1e2754184c3e7bbc2ad3738c3
Maureen Riley <mriley@adaptigroup.com>
2018-01-26 16:02:58 -0600

    Create Pizza class

---
+++ Pizza/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Pizza
+{
+    public int ID { get; set; }
+
+    public List<string> Toppings { get; set; }
+
+    public string Crust { get; set; }
+
+    public string Size { get; set; }
+
+    public double Price { get; set; }
+
+
+    public Pizza()
+	{
+	}
+}
